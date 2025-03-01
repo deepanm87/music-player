990936e836f5b83e9e1c2968c5eaaf7c2f38df62
deepanm87 <deepan.murugan29@gmail.com>
2025-02-28 20:39:29 -0500

    added functionality for play/pause and prevsong/next song

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
+
+const songs = [
+    {
+        name: 'song-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jethro',
+        album: 'album-1'
+    },
+    {
+        name: 'song-2',
+        displayName: 'Seven Nation Army',
+        artist: 'White Lotus',
+        album: 'album-2'
+    }, 
+    {
+        name: 'song-3',
+        displayName: 'Black Mass',
+        artist: 'The Rookie',
+        album: 'album-3'
+    },
+    {
+        name: 'song-4',
+        displayName: 'Atom Peace',
+        artist: 'Radiohead',
+        album: 'album-4'
+    }
+]
 
 let isPlaying = false
 
@@ -22,3 +52,33 @@ function pauseSong() {
 playBtn.addEventListener('click', () => {
     (isPlaying ? pauseSong() : playSong())
 })
+
+function loadSong(song) {
+    title.textContent = `${song.displayName}`
+    artist.textContent = song.artist
+    music.src = `music/${song.name}.mp3`
+    image.src = `img/${song.album}.jpg`
+}
+
+let songIndex = 0
+function prevSong() {
+    songIndex--
+    if(songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+function nextSong() {
+    songIndex++
+    if(songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+loadSong(songs[songIndex])
+
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
