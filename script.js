363edcc4c5ab715c19066e0d29ef713158c2188b
deepanm87 <deepan.murugan29@gmail.com>
2025-03-03 15:19:06 -0500

    added progress bar functionality

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const image = document.querySelector('img')
 const title = document.getElementById('title')
 const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
@@ -58,5 +60,14 @@ function nextSong() {
 
 loadSong(songs[songIndex])
 
+function updateProgressBar(e) {
+    if(isPlaying) {
+        const {duration, currentTime} = e.srcElement
+        const progressPercent = (currentTime / duration) * 100
+        progress.style.width = `${progressPercent}%`
+    }
+}
+
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('timeupdate', updateProgressBar)
